4f40710ed57186c57618a9801de30b4c88079007
kuma.jung <yj.jung5078@gmail.com>
2024-10-27 00:54:10 +0900

    modify deployment shell and buildspec

--- deployment.sh
+++ deployment.sh
@@ -10,18 +10,25 @@ service_java_home="/usr/lib/jvm/java-17-amazon-corretto.x86_64"
 service_java_opt="-XX:+UseG1GC -Xms1G -Xmx1G -Dfile.encoding=UTF-8 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$service_log_path/java_pid.hprof"
 s3_jar_path=JAR_PATH
 
+echo "Install Java 17 to $service_java_home"
+dnf update -y && dnf install -y java-17-amazon-corretto-devel wget git
+
+echo "Create Group and User : $service_name"
 groupadd --system $service_group
 useradd --system --no-create-home --shell /bin/false --gid $service_group $service_user
 
+echo "Create service_path and log path : $service_path, $service_log_path"
 mkdir -p $service_path 
 mkdir -p $service_log_path
 chown -R $service_user:$service_group $service_path
 chown -R $service_user:$service_group $service_log_path
 
 # 배포할 jar 다운로드
+echo "Download s3 to service path"
 aws s3 cp s3://tmc-sample-deployment/$s3_jar_path $service_jar
 
 # systemd 파일 생성
+echo "Create $service_name.service for systemd"
 cat <<EOF > /etc/systemd/system/$service_name.service
 [Unit]
 Description=$service_name service
@@ -44,7 +51,10 @@ EOF
 
 chown root:root "/etc/systemd/system/$service_name.service"
 
+
 #service 등록
+echo "Enable $service_name.service"
 systemctl enable $service_name
 #service 시작
+echo "Start $service_name.service"
 systemctl restart $service_name
